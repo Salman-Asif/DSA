899cb89d3da0b892a16c9782f8b233d5d91bb167
Salman Asif S <37960662+Salman-Asif@users.noreply.github.com>
2023-07-07 00:06:16 +0530

    Create subarray-sum-equals-k.java

---
+++ src/com/dsa/hashing/prefix-sum/subarray-sum-equals-k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int count =0;
+        map.put(0,1);
+
+        for(int i=0;i<nums.length;i++) {
+            sum = sum + nums[i];
+
+          //IMPORTANT: First perform CHECK if it already exists previously or else
+          //you get extra 'count' everytime subarray sum, this is because we want to if the sum existed previously, if yes add to count
+            if(map.containsKey(sum-k)) {
+                count = count + map.get(sum-k);
+            }
+            
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+
+        return count;
+    }
+}
